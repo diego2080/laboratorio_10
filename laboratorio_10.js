8caadb331a1a8b5b1826410f9aab366ea0f0c322
Diego <diegohurtadopadilla3@gmail.com>
2024-10-26 09:54:44 -0500

    commit inicial subida de index.html

---
+++ laboratorio_10.js
@@ -0,0 +1,61 @@
+let nombre = "Carlos";
+let edad = 30;
+let esEstudiante = false;
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Es estudiante:", esEstudiante);
+
+
+let nombre1 = "Diego";
+let edad1 = 19;
+let es1Estudiante = true;
+console.log("Nombre:", nombre1);
+console.log("Edad:", edad1);
+console.log("Es estudiante:", es1Estudiante);
+
+
+
+
+
+
+let a = 8
+let b = 2
+
+Suma=a+b
+Resta=a-b
+Multi=a*b
+Divicion=a/b
+
+console.log("Valor de la variable a es " + a)
+console.log("Valor de la variable b es " + b)
+console.log("La suma de los valores es "+ Suma)
+console.log("La resta de los valores es " + Resta)
+console.log("La multiplicacion de los valores es " + Multi)
+console.log("La divicion de los valores es " + Divicion)
+
+console.log("a igual que b ", a==b )
+console.log("a difente de b", a!=b)
+console.log("a mayor que b", a>b)
+console.log("a menor que b", a<b)
+console.log("a mayor igual que b", a>=b)
+console.log("a menor igual que b", a<=b)
+
+
+
+
+
+let EsMayorDeEdad=true
+let TieneLicencia=false
+
+PuedeManejar=(EsMayorDeEdad && TieneLicencia)
+puedeComprarauoto=(EsMayorDeEdad||TieneLicencia)
+
+console.log("Puede manejar",PuedeManejar)
+console.log("Puede comprar",puedeComprarauoto)
+
+console.log(`Bienvenido. ¿La perosna es puede manejar ${PuedeManejar} ?`)
+
+
+
+
+
